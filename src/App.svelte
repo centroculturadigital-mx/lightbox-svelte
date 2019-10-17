7142710a360435e6ce7b427ad4314ea013489bdc
furenku <furenku@gmail.com>
2019-10-17 00:18:37 -0500

    include svelte-carousel

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,22 @@
 <script>
-	export let name;
+
+    import LightBox from "./LightBox.svelte"
+
+    let images = Array(10).fill(1).map(()=>({
+		url: "http://unsplash.it/300/"+parseInt((Math.random()*200)+200),
+		title: "Minim in sunt aliquip adipisicing excepteur non."
+	}))
+
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
 
-<h1>Hello {name}!</h1>
+<ul class="Imagenes">
+
+    {#each images as image,i ("imagen_"+i)}
+		<img src={image.url} alt={image.title}/>
+    {/each}
+
+</ul>
+
+
+<LightBox content={images}/>
---
+++ src/LightBox.svelte
@@ -0,0 +1,83 @@
+<script>
+
+    import Carousel from "@centroculturadigital-mx/svelte-carousel";
+
+    export let content;
+    let classBase = "LightBox"
+    let classList =  classBase
+    
+    // let classList =  `${classBase} ${classBase}--hidden`
+
+    $: contents = Array.isArray(content) ? content : [content]
+
+</script>
+
+
+<style>
+
+
+
+
+    .LightBox {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+    }
+    
+    .LightBox :global(.carousel),
+    .LightBox :global(.slides) {
+        width: 100vw;
+        height: 100vh;
+    }
+    .LightBox :global(.slides > * > *) {
+
+        width: 100vw !important;
+        height: 100vh !important;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+    .Lightbox__Overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: rgba(0,0,0,0.7);
+        opacity: 1;
+        transition: opacity 1s;
+    }
+
+    .Lightbox--hidden {
+        display: none;
+        opacity: 0;
+    }
+    
+
+
+</style>
+
+
+
+<div class={classList}>
+
+    <div class="Lightbox__Overlay"></div>
+    <Carousel>
+            
+        {#each contents as content,i ("content_"+i)}
+            {#if content.type=="image"|| !content.type }
+                <img src={content.url} alt={content.title}>
+            {/if}
+        {/each}
+
+    </Carousel>
+    
+
+</div>
